5e9739b95a675a85322bc43ceeb371f8c41d21b2
EnakNalla <121622489+EnakNalla@users.noreply.github.com>
2024-03-31 23:08:24 +0100

    nightly backup

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,22 +1,38 @@
 return {
   {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 100,
     config = function()
-      require("catppuccin").setup({
-        flavour = "macchiato",
-        term_colors = true,
-        no_italic = false,
-        no_bold = false,
-        transparent_background = false,
-      })
+      require("poimandres").setup()
     end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "poimandres",
     },
   },
 }
+-- return {
+--   {
+--     "catppuccin/nvim",
+--     lazy = true,
+--     name = "catppuccin",
+--     config = function()
+--       require("catppuccin").setup({
+--         flavour = "macchiato",
+--         term_colors = true,
+--         no_italic = false,
+--         no_bold = false,
+--         transparent_background = false,
+--       })
+--     end,
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
