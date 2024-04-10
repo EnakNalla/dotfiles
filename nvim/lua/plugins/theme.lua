c7a99b1ab3687df69ef95e76fbb034623f81e84c
EnakNalla <121622489+EnakNalla@users.noreply.github.com>
2024-04-10 23:00:05 +0100

    nightly backup

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -9,7 +9,7 @@ return {
         term_colors = true,
         no_italic = false,
         no_bold = false,
-        transparent_background = false,
+        transparent_background = true,
       })
     end,
   },
--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -2,19 +2,19 @@
 
 # poimandres
 
-export RED=0xffd0679d
-export GREEN=0xff5DE4c7
-export YELLOW=0xfffffac2
-export BLUE=0xffADD7FF
-export MAGENTA=0xfff087bd
-export CYAN=0xff91B4D5
-export WHITE=0xfff1f1f0
-export GRAY=0xffa6accd
+export RED=0xffed8796
+export GREEN=0xffa6da95
+export YELLOW=0xffeed49f
+export BLUE=0xff8aadf4
+export MAGENTA=0xffed8796
+export CYAN=0xffc6a0f6
+export WHITE=0xffcad3f5
+export GRAY=0xff6e738d
 
-export BAR_COLOR=0xff1b1e28
+export BAR_COLOR=0xff24273a
 
-export SELECTED_COLOR=0xff717cb425
-export FOREGROUND=0xffe4f0fb
+export SELECTED_COLOR=0xffc6a0f6
+export FOREGROUND=0xff494d64
 
 # Color Palette
 export BLACK=0xff181926
@@ -34,8 +34,8 @@ export TRANSPARENT=0x00000000
 export BAR_BORDER_COLOR=$SELECTED_COLOR
 export ICON_COLOR=$WHITE  # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0x603c3e4f
-export BACKGROUND_2=0x60494d64
+export BACKGROUND_1=0xff1e2030
+export BACKGROUND_2=0xff181926
 
 export POPUP_BACKGROUND_COLOR=$BAR_COLOR
 export POPUP_BORDER_COLOR=$WHITE
